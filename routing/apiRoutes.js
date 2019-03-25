2f9c58796d0c591e8298be24d5b92b36f2b82425
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-03-25 13:47:21 -0700

    update: algo to find a match

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,14 +10,40 @@ routes.get('/friends', function(req, res) {
 });
 
 routes.post('/friends', function(req, res) {
-    //TODO: Add logic to append a new friend to the frineds list
-    //TODO: Add logic to match friends
+    //DONE: Add logic to append a new friend to the frineds list
+    //TODO: Add logic to match friends  
+    let friendMatch = '';
+    let frinedPhoto = '';
+    let matchDiff = 55; //the greates diff
 
     console.log('Posting to friends');
     console.log(req.body);
 
-    friends.push(req.body);
+    // Add logic to find friend match here
 
+    friends.forEach(function(friends) {
+        let matchScores = [];
+        let totalDiff = 55;
+
+        function sum(total, num) {
+            return total + num;
+        }
+
+        for(let i = 0; i < friends.scores.length; i++) {
+            matchScores.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friends.scores[i])));
+        }
+
+        totalDiff = matchScores.reduce(sum, 0);
+
+        if(totalDiff < matchDiff) {
+            matchDiff = totalDiff;
+            friendMatch = friends.name;
+            frinedPhoto = friends.photo;
+        }
+    });
+    // Add logic to find friend match here 
+
+    friends.push(req.body);// push new user data to records. do this after finding the match
     res.send('Posted to Friends');
 });
 
